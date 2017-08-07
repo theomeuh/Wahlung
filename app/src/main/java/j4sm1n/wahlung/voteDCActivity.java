5d181d88a410c587c5d04f2fa0aeeb806339b6fd
theomeuh <cnimporteki@hotmail.fr>
2017-08-07 19:55:01 +0200

    previous bugs debugged
    app is almost functional

--- app/src/main/java/j4sm1n/wahlung/voteDCActivity.java
+++ app/src/main/java/j4sm1n/wahlung/voteDCActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class voteDCActivity extends AppCompatActivity implements View.OnClickListener {
@@ -27,8 +25,7 @@ public class voteDCActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_dc);
 
-        listeChoix = new ArrayList<String>();
-        ArrayList<String> listeChoix = getIntent().getStringArrayListExtra("liste");
+        listeChoix = getIntent().getStringArrayListExtra("liste");
 
         combatPossible = Traitement.combatPossible(listeChoix);
         nb_duel = combatPossible.length ;
@@ -44,8 +41,7 @@ public class voteDCActivity extends AppCompatActivity implements View.OnClickLis
         resultbutton = (Button) findViewById(R.id.resultbutton);
         resultbutton.setOnClickListener(this);
 
-        buttonGauche.setText(combatPossible[compteur_duel][0]);
-        buttonDroite.setText(combatPossible[compteur_duel][1]);
+        this.Initialize();
     }
     public void onClick (View v){
         compteur_duel+=1;
@@ -53,27 +49,23 @@ public class voteDCActivity extends AppCompatActivity implements View.OnClickLis
             switch(v.getId()){
                 case R.id.buttonGauche:
                     duel_gagne[compteur_duel]+=1;
-                    buttonGauche.setText(combatPossible[compteur_duel][0]);
-                    buttonDroite.setText(combatPossible[compteur_duel][1]);
                 break;
                 case R.id.buttonDroite:
-                    duel_gagne[nb_duel]-=1;
-                    buttonGauche.setText(combatPossible[compteur_duel][0]);
-                    buttonDroite.setText(combatPossible[compteur_duel][1]);
+                    duel_gagne[compteur_duel]-=1;
                 break;
             }
+            buttonGauche.setText(combatPossible[compteur_duel][0]);
+            buttonDroite.setText(combatPossible[compteur_duel][1]);
         }
         else{
             switch (v.getId()){
                 case R.id.clearbutton:
-                    compteur_duel=0;
-                    buttonGauche.setText(combatPossible[compteur_duel][0]);
-                    buttonDroite.setText(combatPossible[compteur_duel][1]);
+                    this.Initialize();
                 break;
                 case R.id.resultbutton:
                     int nb_winner = Traitement.findingWinner(duel_gagne);
                     Intent intent = new Intent(voteDCActivity.this, AffichageResult.class);
-                    intent.putExtra("winner",listeChoix.get(nb_winner));
+                    intent.putExtra("winner", listeChoix.get(nb_winner));
                     startActivity(intent);
                 break;
                 default:
@@ -81,5 +73,10 @@ public class voteDCActivity extends AppCompatActivity implements View.OnClickLis
                     buttonDroite.setText("fini");
             }
         }
+    }
+    private void Initialize(){
+        compteur_duel = 0;
+        buttonGauche.setText(combatPossible[0][0]);
+        buttonDroite.setText(combatPossible[0][1]);
     }
 }
